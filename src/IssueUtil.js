6d2aab2f4c358a2db298403f07218efc091f53dc
omgitsbillryan <bill.ryan@adhocteam.us>
2021-09-24 14:51:20 -0400

    debug logging

--- src/IssueUtil.js
+++ src/IssueUtil.js
@@ -10,16 +10,20 @@ module.exports = class IssueUtil {
   }
 
   getIssueBody(id, repository) {
+    console.log(repository)
     const [owner, repo] = repository.split('/');
     const target_repo = repository ? {owner, repo} : github.context.repo;
-    
+    console.log('about to start')
+    console.log(target_repo)
     return this.octokit.issues.get({
       ...target_repo,
       issue_number: id
     }).then(result => {
+      console.log(result.status)
       if (result.status !== 200) {
         throw new Error(`Unexpected status code from retrieving issue: ${result.status}`);
       }
+      console.log(result.data.body)
       return result.data.body;
     }).catch(err => {
       throw err;
